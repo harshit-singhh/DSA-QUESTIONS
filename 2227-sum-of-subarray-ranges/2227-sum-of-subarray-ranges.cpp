6b392fb7b43d86e204a0420a7230bceab6eeb955
Harshit Singh <harshitsingh81203@gmail.com>
2023-07-01 12:18:18 +0530

    Time: 24 ms (63.23%), Space: 12.3 MB (6.97%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
@@ -1,23 +1,123 @@
 class Solution {
 public:
-    long long subArrayRanges(vector<int>& arr) {
+
+     vector<int> findNextLarger(vector<int>arr,int n ){
+        vector<int>final(n);
+        stack<int>st;
         
-        long long sum = 0;
-        int n = arr.size();
-        for(int i = 0 ; i < n ; i ++){
+        for(int i = n-1; i >= 0 ; i --){
+            while(!st.empty() && arr[i] > arr[st.top()]){
+                st.pop();
+            }
             
-            int maxi = arr[i];
-            int mini = arr[i];
-            
-            for(int j = i ; j < n ; j ++){
-                
-                if(arr[j] > maxi) maxi = arr[j];
-                if(arr[j] < mini) mini = arr[j];
-                
-                sum+=(maxi-mini);
+            if(!st.empty()){
+                final[i] = st.top();
+                st.push(i);
+            }
+            else{
+                final[i] = n;
+                st.push(i);
             }
         }
         
+        return final;
+        
+        
+    }
+    
+    vector<int> findPrevLarger(vector<int> arr, int n){
+        vector<int>final(n);
+        stack<int>st;
+        
+        for(int i = 0; i < n ; i ++){
+            while(!st.empty() && arr[i] >= arr[st.top()]){
+                st.pop();
+            }
+            
+            if(!st.empty()){
+                final[i] = st.top();
+                st.push(i);
+            }
+            else{
+                final[i] = -1;
+                st.push(i);
+            }
+        }
+        
+        return final;
+    }
+    
+    vector<int>findNextSmaller(vector<int>arr , int n){
+        vector<int>final(n);
+        stack<int>st;
+        
+        for(int i = n-1; i >= 0 ; i --){
+            while(!st.empty() && arr[i] < arr[st.top()]){
+                st.pop();
+            }
+            
+            if(!st.empty()){
+                final[i] = st.top();
+                st.push(i);
+            }
+            else{
+                final[i] = n;
+                st.push(i);
+            }
+        }
+        
+        return final;
+    }
+    
+    vector<int>findPrevSmaller(vector<int>arr, int n){
+        vector<int>final(n);
+        stack<int>st;
+        
+        for(int i = 0; i < n ; i ++){
+            while(!st.empty() && arr[i] <= arr[st.top()]){
+                st.pop();
+            }
+            
+            if(!st.empty()){
+                final[i] = st.top();
+                st.push(i);
+            }
+            else{
+                final[i] = -1;
+                st.push(i);
+            }
+        }
+        
+        return final;
+
+    }
+    long long subArrayRanges(vector<int>& arr) {
+
+        int n = arr.size();
+
+        vector<int>nextLarger = findNextLarger(arr,n);
+        vector<int>prevLarger = findPrevLarger(arr,n);
+        vector<int>nextSmaller = findNextSmaller(arr,n);
+        vector<int>prevSmaller = findPrevSmaller(arr,n);
+        
+        
+        long long sum = 0;
+        
+        for(int i = 0 ; i < n ; i ++){
+            long long LARGER = (nextLarger[i] - i)*(i - prevLarger[i]);
+            long long SMALLER = (nextSmaller[i] - i)*(i - prevSmaller[i]);
+
+
+            long long to_add = arr[i]*LARGER;
+            long long to_sub = arr[i]*SMALLER;
+            
+            
+            sum+= to_add;
+            sum-= to_sub;
+        }
+        
         return sum;
+        
+        
     }
 };
