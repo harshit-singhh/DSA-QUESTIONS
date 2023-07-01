7551ee6ff6cb8177d75cee25191bdc430e4639d8
Harshit Singh <harshitsingh81203@gmail.com>
2023-07-01 18:02:02 +0530

    Time: 515 ms (48.03%), Space: 175.5 MB (39.29%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,49 +1,72 @@
 class LRUCache {
 public:
-     list<int>dll; 
-        map<int, pair<list<int>::iterator , int>>mp; // ye address ko likhne ka method he, jese inteager ko likhne ka int hota he.
-                                                    //list<int>::iterator
-        int n;
-        LRUCache(int capacity){
-            n = capacity;
-        }
-    
-        void makeRecentlyUsed(int key){
-            dll.erase(mp[key].first);
-             
-            dll.push_front(key);
-            
-            mp[key].first = dll.begin(); // this gives the new address of the first element of the list.
-        }
-    
-        int get(int key){
-            if(mp.find(key) == mp.end()) return -1;
-        
-            makeRecentlyUsed(key); // is function se apni key first pe aa jaegi.
-        
-            return mp[key].second;
-        }
-    
-        void put(int key,int value){
-            
-            if(mp.find(key) != mp.end()){
-                mp[key].second = value;
-                makeRecentlyUsed(key);
-            }
-            
-            else{
-                dll.push_front(key);
-                mp[key] = {dll.begin(),value};
-                n--;
-            }
-            
-            if(n<0){ // mtlb ek element extra daal diya
-                int key_in_map = dll.back();
-                mp.erase(key_in_map);
-                //now delete from list also
-                dll.pop_back();
-                n++;
-            }
-        }
-};
 
+// Make a class of Node which contain key,value,Prev ptr,next ptr.
+class Node{
+public:
+    int key,val;
+    Node* prev;
+    Node* next;
+    Node(int _key,int _val){
+        key=_key;
+        val=_val;
+    }
+};
+int cap; // to check the capacity of the map
+
+unordered_map<int,Node*>mp; // make a map of int and Node* type , basically Node* stores the address of the node
+
+Node* head=new Node(-1,-1); // make a head with value -1,-1.
+Node* tail=new Node(-1,-1); // make a tail with value -1,-1.
+
+    LRUCache(int capacity) {
+        cap=capacity; // capacity scope in only in this loop so store it in cap
+        head->next=tail; // make a link head->tail and tail->prev becaue of DLL(double Linked list)
+        tail->prev=head;
+    }
+// Do dry for better understanding
+
+    // I need to add newNode right after the head,
+    void addNode(Node* newNode){
+        Node* temp=head->next; // store the head next 
+        // and then made 4 links, because of dll
+        head->next=newNode;
+        newNode->prev=head;
+        newNode->next=temp;
+        temp->prev=newNode;
+    }
+
+    void deleteNode(Node* delNode){
+        // store prev of delNode and nextNode and then delete and form the link For DLL.
+        Node* delPrev=delNode->prev;
+        Node* delNext=delNode->next;
+        delPrev->next=delNode->next;
+        delNext->prev=delNode->prev;
+    }
+
+    int get(int key) {
+        if(mp.find(key)!=mp.end()){ // if it is present in map
+            Node* resNode=mp[key]; // store the address of mp of key
+            int res=resNode->val; // store the val of the address
+            deleteNode(resNode); // delete that node
+            addNode(resNode); // add that node 
+            mp[key]=head->next; // now mp of key contain new address
+            return res;
+        }   
+        return -1;  // if not present in map
+    }
+    
+    void put(int key, int value) {
+        if(mp.find(key)!=mp.end()){ // if it is already in map 
+            Node* existingNode=mp[key]; // fin d the address
+            mp.erase(key);// erase it from map
+            deleteNode(existingNode); // delete it 
+        }
+        if(mp.size()==cap){ // it size equals delete the LRU node which is at tail->prev
+            mp.erase(tail->prev->key); 
+            deleteNode(tail->prev);
+        }
+        addNode(new Node(key,value)); // add then new node with the key,value right after head
+        mp[key]=head->next; // change the new address of map of key with head->next
+    }
+};
