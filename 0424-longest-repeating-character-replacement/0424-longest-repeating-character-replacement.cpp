d086ab9a257a25c8cc0c784cf1f71186dc9ec83d
Harshit Singh <harshitsingh81203@gmail.com>
2023-07-04 22:28:59 +0530

    Time: 24 ms (26.26%), Space: 6.9 MB (76.26%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,42 +1,65 @@
 class Solution {
 public:
 
-    int maxFrequentValue(unordered_map<char,int>&m){
+    // int maxFrequentValue(unordered_map<char,int>&m){
         
-        int maxi = 0;
+    //     int maxi = 0;
         
-        for(auto i : m){
-            maxi = max(maxi , i.second);
-        }
+    //     for(auto i : m){
+    //         maxi = max(maxi , i.second);
+    //     }
         
-        return maxi;
-    }
+    //     return maxi;
+    // }
 
-    
+
+    // int characterReplacement(string S, int K) {
+    //     int left = 0 , right = 0 ;
+    //     int maxlength = 0;
+        
+    //     unordered_map<char,int>m;
+        
+    //     while(right < S.size()){
+            
+            
+    //         m[S[right]]++;
+
+    //         while(((right - left +1) - maxFrequentValue(m)) > K){
+    //             m[S[left]]--;
+                
+    //             left++;
+    //         }
+                
+
+    //         int windowLength = right-left+1;
+    //         maxlength = max(maxlength,windowLength);
+    //         right++;
+
+    //     }
+        
+    //     return maxlength;
+
+
     int characterReplacement(string S, int K) {
-        int left = 0 , right = 0 ;
-        int maxlength = 0;
+        int n = S.size();
+        vector<int> mp(26, 0);
+        int head = 0; // right
+        int tail = 0; // left
+        int res = 0;
         
-        unordered_map<char,int>m;
-        
-        while(right < S.size()){
+        for(int head = 0; head < n; head++) {
+            mp[S[head] - 'A']++;
             
-            
-            m[S[right]]++;
-
-            while(((right - left +1) - maxFrequentValue(m)) > K){
-                m[S[left]]--;
-                
-                left++;
+            while((head - tail + 1) - *max_element(mp.begin(), mp.end()) > K) {
+                mp[S[tail] - 'A']--;
+                tail++;
             }
-                
-
-            int windowLength = right-left+1;
-            maxlength = max(maxlength,windowLength);
-            right++;
-
+            
+            res = max(res, (head - tail + 1));
         }
         
-        return maxlength;
+        return res;
+    
+    
     }
 };
