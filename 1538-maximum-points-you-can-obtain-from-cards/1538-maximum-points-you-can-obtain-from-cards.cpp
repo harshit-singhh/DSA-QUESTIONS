4745c5b4a09dcfa4ebbe0776796dc10b4e31c633
Harshit Singh <harshitsingh81203@gmail.com>
2023-07-05 13:29:32 +0530

    Time: 72 ms (44.02%), Space: 45.7 MB (6.93%) - LeetHub

---
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int maxScore(vector<int>& arr, int k) {
+
+        int n = arr.size();
+
+        if(k == n) return accumulate(arr.begin(), arr.end(),0);
+        if(k>n) return -1;
+        
+        vector<long long>SumAteachIndex(n);
+        
+        long long sum = 0;
+        for(int i = 0 ; i < n ; i++){ //prefix sum
+            sum += arr[i];
+            SumAteachIndex[i] = sum;
+        }
+
+        long long totalsumOfArray = SumAteachIndex[n-1];
+        
+        int skipWindow = n - k; // jesi arr me 10 element he and humko 4 select karke max sum nikalna he then 
+        // 6 window hmesha bach jaengi. jiska sum hmare matlab ka nhi he.
+        
+        long long maxSum = 0;
+        
+        int left = 0, right = skipWindow - 1;
+        
+        while(right < n){
+            
+            long long ImpSum = 0;
+            
+            if(left == 0 ){
+                ImpSum = totalsumOfArray - (SumAteachIndex[right]);
+            }
+            else{
+                ImpSum = totalsumOfArray - (SumAteachIndex[right] - SumAteachIndex[left-1]); // dry run kroge to smj jaoge
+            }
+            
+            left++;
+            right++;
+            
+            maxSum = max(maxSum, ImpSum);
+        }
+        
+        return maxSum;
+        
+    }
+};
