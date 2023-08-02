921984c394f74e634eec8486987346c420b2f47a
Harshit Singh <harshitsingh81203@gmail.com>
2023-08-02 14:50:18 +0530

    Time: 89 ms (47.84%), Space: 28.5 MB (16.22%) - LeetHub

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,41 @@
+class Solution {
+    int solve( string &s1 , string &s2 , int n , int m ,vector<vector<int>>&dp){
+        
+        if(n == -1) return 0;
+        if(m == -1) return 0;
+        
+        if(dp[n][m] != -1) return dp[n][m];
+        
+        if(s1[n] == s2[m]){
+            return dp[n][m] = 1 + solve(s1 , s2 , n-1 , m-1 ,dp);
+        }
+        
+        else return dp[n][m] = max(solve(s1 , s2 , n-1 , m ,dp) , solve(s1 ,s2 , n , m-1 ,dp));
+    }
+    
+    
+    public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size();
+        int m = text2.size();
+        vector<vector<int>>dp(n+1 , vector<int>(m+1 , -1));
+        return solve(text1 , text2 , n-1 , m-1 ,dp);
+    }
+    
+    
+     public:
+    int longestPalinSubseq(string A) {
+        
+        
+        // just usi string ko reverse karke LCS  wale question ke same code me daal diya
+        string T = A;
+        reverse(T.begin(), T.end());
+        return longestCommonSubsequence(A, T);
+        
+        
+    }
+public:
+    int minInsertions(string s) {
+        return s.size() - longestPalinSubseq(s);
+    }
+};
