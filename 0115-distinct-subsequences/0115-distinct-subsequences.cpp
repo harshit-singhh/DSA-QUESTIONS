c2cafbd03726201ecad9ed8e1ae95d0e6670d7a3
Harshit Singh <harshitsingh81203@gmail.com>
2023-08-03 22:32:54 +0530

    Time: 39 ms (78.11%), Space: 42.1 MB (19.11%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -11,12 +11,40 @@ class Solution {
         }
         return dp[i][j] = solve(s1 ,s2 , i-1 , j ,dp);
     }
+
+
+    //tabulation
+
+    int solvetab(string s1 ,string s2 , int n ,int m){
+        vector<vector<double>>dp(n+1 , vector<double>(m+1 , -1));
+        for(int i = 0 ; i <= m ; i++){
+            dp[0][i] = 0;
+        }
+        for(int i = 0 ; i <= n ;i++){
+            dp[i][0] = 1;
+        }
+
+
+        for(int i = 1 ; i<= n ; i++){
+            for(int j = 1 ; j<= m ; j++){
+                if(s1[i-1] == s2[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+
+                }
+                else dp[i][j] = dp[i-1][j];  
+            }
+        }
+
+        return (int)dp[n][m];
+    }
+
 public:
     int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size();
 
         vector<vector<int>>dp(n+1 , vector<int>(m+1 , -1));
-        return solve(s, t , n-1 , m-1 ,dp);
+        // return solve(s, t , n-1 , m-1 ,dp);
+        return solvetab(s, t , n , m);
     }
 };
