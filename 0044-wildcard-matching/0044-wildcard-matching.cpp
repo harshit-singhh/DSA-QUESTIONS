9548ed0c04786c0b956bde4a094f49f781e27972
Harshit Singh <harshitsingh81203@gmail.com>
2023-08-04 17:25:54 +0530

    Time: 35 ms (93.54%), Space: 27.9 MB (22.57%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -21,6 +21,40 @@ class Solution {
         }
         else return dp[i][j] = false;
     }
+
+
+    //tabulation
+
+    bool solvetab(string str1 , string str2 , int n , int m){
+        vector<vector<int>>dp(n+1 , vector<int>(m+1 , -1));
+
+        for(int i = 1; i<= n ; i++){
+            dp[i][0] = false;
+        }
+        dp[0][0] = true;
+        for(int i = 0 ; i< m ; i++){
+            if(str2[i] != '*') dp[0][i+1] = false;
+            else dp[0][i+1] = true;
+        }
+
+
+        for(int i = 1 ; i<= n ; i++){
+            for(int j = 1 ; j<= m ; j++){
+                if(str1[i-1] == str2[j-1] || str2[j-1] == '?'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if(str2[j-1] == '*'){
+                    dp[i][j] = (dp[i-1][j-1] || dp[i][j-1] || dp[i-1][j]);
+                }
+                else dp[i][j] = false;
+            }
+        }
+
+        return dp[n][m];
+
+    }
+
+
 public:
     bool isMatch(string s, string p) {
         
@@ -29,5 +63,6 @@ public:
 
         vector<vector<int>>dp(n+1 , vector<int>(m+1 , -1));
         return solve(s , p , n-1 , m-1 ,dp);
+        return solvetab(s , p , n , m);
     }
 };
