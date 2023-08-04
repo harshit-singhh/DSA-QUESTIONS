a75f36e2f9275eece411ffab27f83ee637e7d99c
Harshit Singh <harshitsingh81203@gmail.com>
2023-08-04 14:28:33 +0530

    Time: 20 ms (40.3%), Space: 11.8 MB (20.02%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -16,6 +16,33 @@ class Solution {
                             solve(str1 , str2 , i-1 , j ,dp)));
         }
     }
+
+
+    // tabulation
+
+    int solvetab(string str1 , string str2 , int n , int m){
+        vector<vector<int>>dp(n+1 , vector<int>(m+1, -1));
+        for(int i = 0 ; i<= n ; i++){
+            dp[i][0] = i+1;
+        }
+
+        for(int i = 0 ; i<= m ; i++){
+            dp[0][i] = i+1;
+        }
+
+        for(int i = 1 ; i<= n ; i++){
+            for(int j = 1 ; j<= m ; j++){
+                if(str1[i-1] == str2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = 1 + min(dp[i][j-1] , min(dp[i-1][j-1] , dp[i-1][j]));
+                }
+            }
+        }
+
+        return dp[n][m]-1;
+    }
 public:
     int minDistance(string word1, string word2) {
         
@@ -23,6 +50,7 @@ public:
         int m = word2.size();
 
         vector<vector<int>>dp(n+1 , vector<int>(m+1, -1));
-        return solve(word1 , word2 , n-1 , m-1 ,dp);
+        // return solve(word1 , word2 , n-1 , m-1 ,dp);
+        return solvetab(word1 , word2 , n , m );
     }
 };
