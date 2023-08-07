f52b6e30fdb90c1ebb1d06ceb6a986d2ccc50fab
Harshit Singh <harshitsingh81203@gmail.com>
2023-08-07 12:16:46 +0530

    Time: 4 ms (24.52%), Space: 6.7 MB (19.19%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -15,11 +15,37 @@ class Solution {
         
         
     }
+
+    // tabulation
+
+    int solve(int m , int n ){
+        vector<vector<int>>dp(m , vector<int>(n+1 , -1));
+
+        dp[0][0] = 1;
+
+        for(int row = 0 ; row< m ; row++){
+            for(int col = 0 ; col < n ; col++){
+                if(row == 0 && col == 0 )continue;
+                int ways1 = 0;
+                if(row -1 >= 0)
+                ways1 = dp[row -1][col];
+                
+                int ways2 = 0;
+                if(col - 1 >= 0)
+                ways2 = dp[row][col-1];
+                
+                dp[row][col] = (ways1 + ways2);
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
 public:
     int uniquePaths(int m, int n) {
         
         
         vector<vector<int>>dp(m , vector<int>(n , -1));
-        return solve(m-1 , n-1 , dp);
+        // return solve(m-1 , n-1 , dp);
+        return solve(m , n);
     }
 };
