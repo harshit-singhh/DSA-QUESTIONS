1874285eda5c15f5cb42ba00cf985b11b778d8d8
Harshit Singh <harshitsingh81203@gmail.com>
2023-08-07 23:35:05 +0530

    Time: 13 ms (77.59%), Space: 18.9 MB (20.57%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,24 +1,27 @@
 class Solution {
-    int solve(vector<int>&coins , int n , int amount , int index , int sum ,vector<vector<int>>&dp){
-        if(sum == amount) return 1;
-        if(sum > amount) return 0;
-        if(index == n){
-            if(sum == amount) return 1;
+    int solve(vector<int>&coins , int amount , int index , vector<vector<int>>&dp){
+
+        if(index == 0){
+            if(amount % coins[0] == 0){
+                return 1;
+            }
             return 0;
         }
 
-        if(dp[index][sum] != INT_MIN) return dp[index][sum];
-        int pick = solve(coins , n , amount , index, sum + coins[index] ,dp);
-        int notpick = solve(coins , n , amount , index + 1, sum,dp );
+        if(dp[index][amount] != -1) return dp[index][amount];
+        int notpick = solve(coins , amount , index-1, dp);
+        int pick = 0;
+        if(amount - coins[index] >= 0){
+            pick = solve(coins , amount - coins[index] , index ,dp);
+        }
 
-        return dp[index][sum] = pick + notpick;
+        return dp[index][amount] = pick + notpick;
     }
 public:
     int change(int amount, vector<int>& coins) {
-        if(amount == 0) return 1;
         int n = coins.size();
 
-        vector<vector<int>>dp(n+1 , vector<int>(amount+1, INT_MIN));
-        return solve(coins ,n , amount , 0 , 0 ,dp);
+        vector<vector<int>>dp(n+1 , vector<int>(amount+1 , -1));
+        return solve(coins , amount , n-1 ,dp);
     }
 };
