85e75461d500eb93cb87429629c5b4c9f993eb8b
Harshit Singh <harshitsingh81203@gmail.com>
2023-08-07 13:54:59 +0530

    Time: 900 ms (8.04%), Space: 338.7 MB (5.19%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -24,6 +24,42 @@ class Solution {
 
         return dp[index][canbuy][count] = profit;
     }
+
+
+    //tabulation
+
+     int solvetab(vector<int>&prices , int n){
+         vector<vector<vector<int>>>dp(prices.size()+1 , vector<vector<int>>(2 , vector<int>(3,0)));
+        
+        // base case not needed because all values are already zeroes
+
+        for(int index = n-1 ; index >= 0 ; index--){
+            for(int canbuy = 0 ; canbuy <= 1 ; canbuy++){
+                for(int count = 1 ; count <=2 ; count++){
+                     int profit = 0;
+
+                    if(canbuy){
+                        int buy =  -prices[index] + dp[index+1][0][count]; 
+                        int notbuy = 0 + dp[index+1][1][count];
+
+                        profit = max(buy , notbuy);
+                    }
+                    else{
+                        int sell = prices[index] + dp[index+1][1][count -1];
+                        int notsell = 0 + dp[index+1][0][count];
+
+                        profit = max(sell , notsell);
+                    }
+
+                    dp[index][canbuy][count] = profit;
+                }
+
+            }
+        }
+
+        return dp[0][1][2];
+
+     }
 public:
     int maxProfit(vector<int>& prices) {
         int canbuy = 1;
@@ -31,6 +67,7 @@ public:
         vector<vector<vector<int>>>dp(prices.size()+1 , vector<vector<int>>(2 , vector<int>(3, -1)));
 
         int count = 2;
-        return solve(prices , 0 , canbuy , count , dp);
+        // return solve(prices , 0 , canbuy , count , dp);
+        return solvetab(prices , prices.size());
     }
 };
