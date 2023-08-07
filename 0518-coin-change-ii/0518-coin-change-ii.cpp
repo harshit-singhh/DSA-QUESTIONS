e13580a6f19f9d8d52544f3b9552a692b429ef1e
Harshit Singh <harshitsingh81203@gmail.com>
2023-08-07 23:48:05 +0530

    Time: 28 ms (55.63%), Space: 19 MB (15.67%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -45,12 +45,45 @@ class Solution {
         return dp[n-1][target];
     }
 
+
+    int space(vector<int>&coins , int target , int n){
+        vector<int>prev(target+1 , 0);
+        vector<int>curr(target+1 , 0);
+
+        for(int i = 0 ; i <= target ; i++){
+            if(i % coins[0] == 0){
+                prev[i] = 1;
+            }
+            else prev[i] = 0;
+        }
+
+        for(int index = 1 ; index < n ; index++){
+            for(int amount = 0 ; amount <= target ; amount++){
+
+                int notpick = prev[amount];
+                int pick = 0;
+                if(amount - coins[index] >= 0){
+                    pick = curr[amount - coins[index]];
+                }
+
+                curr[amount] = pick + notpick;
+            }
+            prev = curr;
+        }
+
+        return prev[target];
+    
+    }
+
+
+
 public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
 
         vector<vector<int>>dp(n+1 , vector<int>(amount+1 , -1));
         // return solve(coins , amount , n-1 ,dp);
-        return solvetab(coins , amount , n);
+        // return solvetab(coins , amount , n);
+         return space(coins , amount , n);
     }
 };
