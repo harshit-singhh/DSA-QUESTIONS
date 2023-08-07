70bcd4b8978e158b2d397fbecb51280cf2e24f3e
Harshit Singh <harshitsingh81203@gmail.com>
2023-08-08 00:23:28 +0530

    Time: 31 ms (93.44%), Space: 24.6 MB (11.05%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -39,7 +39,26 @@ class Solution {
 
         return dp[n][m];
 
+    }
 
+
+    int space(string s1 , string s2 , int n , int m){
+
+        vector<int>prev(m +1 , 0);
+        vector<int>curr(m + 1 , 0);
+
+        for(int i = 1 ; i<= n ; i++){
+            for(int j = 1 ; j<= m ; j++){
+                 if(s1[i-1] == s2[j-1]){ 
+                    curr[j] = 1 + prev[j-1];
+                }
+        
+                else curr[j] = max(prev[j],curr[j-1]);
+            }
+            prev = curr;
+        }
+
+        return prev[m];
     }
 
 public:
@@ -49,6 +68,7 @@ public:
         vector<vector<int>>dp(n+1 , vector<int>(m+1 , -1));
         // return solve(text1 , text2 , n , m ,dp); // to shift the index . n- 1 = n , m-1 = m
 
-        return solvetab(text1 , text2 , n , m);
+        // return solvetab(text1 , text2 , n , m);
+        return space(text1 , text2 , n , m );
     }
 };
