8a0e8929a26119d9fa81bb6587b3827a66167251
Harshit Singh <harshitsingh81203@gmail.com>
2023-08-09 00:38:47 +0530

    Time: 18 ms (33.93%), Space: 7.2 MB (12.46%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,65 +1,66 @@
 class Solution {
 public:
 
-    // int maxFrequentValue(unordered_map<char,int>&m){
+    int maxFrequentValue(unordered_map<char,int>&m){
         
-    //     int maxi = 0;
+        int maxi = 0;
         
-    //     for(auto i : m){
-    //         maxi = max(maxi , i.second);
-    //     }
+        for(auto i : m){
+            maxi = max(maxi , i.second);
+        }
         
-    //     return maxi;
-    // }
-
-
-    // int characterReplacement(string S, int K) {
-    //     int left = 0 , right = 0 ;
-    //     int maxlength = 0;
-        
-    //     unordered_map<char,int>m;
-        
-    //     while(right < S.size()){
-            
-            
-    //         m[S[right]]++;
-
-    //         while(((right - left +1) - maxFrequentValue(m)) > K){
-    //             m[S[left]]--;
-                
-    //             left++;
-    //         }
-                
-
-    //         int windowLength = right-left+1;
-    //         maxlength = max(maxlength,windowLength);
-    //         right++;
-
-    //     }
-        
-    //     return maxlength;
+        return maxi;
+    }
 
 
     int characterReplacement(string S, int K) {
-        int n = S.size();
-        vector<int> mp(26, 0);
-        int head = 0; // right
-        int tail = 0; // left
-        int res = 0;
+        int left = 0 , right = 0 ;
+        int maxlength = 0;
         
-        for(int head = 0; head < n; head++) {
-            mp[S[head] - 'A']++;
+        unordered_map<char,int>m;
+        
+        while(right < S.size()){
             
-            while((head - tail + 1) - *max_element(mp.begin(), mp.end()) > K) {
-                mp[S[tail] - 'A']--;
-                tail++;
+            
+            m[S[right]]++;
+
+            while(((right - left +1) - maxFrequentValue(m)) > K){
+                m[S[left]]--;
+                
+                left++;
             }
-            
-            res = max(res, (head - tail + 1));
+                
+
+            int windowLength = right-left+1;
+            maxlength = max(maxlength,windowLength);
+            right++;
+
         }
         
-        return res;
-    
-    
+        return maxlength;
     }
+
+
+    // int characterReplacement(string S, int K) {
+    //     int n = S.size();
+    //     vector<int> mp(26, 0);
+    //     int head = 0; // right
+    //     int tail = 0; // left
+    //     int res = 0;
+        
+    //     for(int head = 0; head < n; head++) {
+    //         mp[S[head] - 'A']++;
+            
+    //         while((head - tail + 1) - *max_element(mp.begin(), mp.end()) > K) {
+    //             mp[S[tail] - 'A']--;
+    //             tail++;
+    //         }
+            
+    //         res = max(res, (head - tail + 1));
+    //     }
+        
+    //     return res;
+    
+    
+    // }
 };
