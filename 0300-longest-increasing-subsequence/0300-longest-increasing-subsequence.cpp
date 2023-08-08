09732ac521884b98e9269bdeb5e5e628ce4934ea
Harshit Singh <harshitsingh81203@gmail.com>
2023-08-08 13:16:25 +0530

    Time: 834 ms (29.51%), Space: 292.5 MB (8.1%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -2,19 +2,19 @@ class Solution {
     private:
     
     int solve(vector<int>&nums , int n , int index , int prev ,vector<vector<int>>&dp){
-        if(index == n+1){
+        if(index == n){
             return 0;
         }
 
-        if(dp[index][prev] != -1) return dp[index][prev];
+        if(dp[index][prev+1] != -1) return dp[index][prev+1];
 
         int notpick = 0 + solve(nums , n , index+1 , prev ,dp);
         int pick = 0;
-        if(prev == 0 || nums[index-1] > nums[prev-1]){
+        if(prev == -1 || nums[index] > nums[prev]){
             pick = 1 + solve(nums , n , index+1 , index ,dp);
         }
 
-        return dp[index][prev] = max(pick , notpick);
+        return dp[index][prev+1] = max(pick , notpick);
     }
 public:
     int lengthOfLIS(vector<int>& nums) {
@@ -23,6 +23,6 @@ public:
 
         vector<vector<int>>dp(n+1 , vector<int>(n+1 , -1));
         
-        return solve(nums , n , 1 , 0 ,dp);
+        return solve(nums , n , 0 , -1 ,dp);
     }
 };
