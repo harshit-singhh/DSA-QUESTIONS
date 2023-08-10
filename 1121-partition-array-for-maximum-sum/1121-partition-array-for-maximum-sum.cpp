d5e2f87a40aaca40192b20dbeb6dd5051c25b675
Harshit Singh <harshitsingh81203@gmail.com>
2023-08-10 20:51:49 +0530

    Time: 8 ms (98.22%), Space: 8.7 MB (6.45%) - LeetHub

--- 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.cpp
@@ -15,10 +15,29 @@ class Solution {
         }
         return dp[i] = maxprofit;
     }
+
+    int solvetab(vector<int>&arr, int n , int k){
+        vector<int>dp(n+1 , 0);
+
+        for(int i = n-1 ; i >= 0 ; i--){
+            int maxelement = INT_MIN;
+            int maxprofit = 0;
+            int profit = 0;
+             for(int j = i ; j<min(i+k , n)  ; j++ ){
+                maxelement = max(maxelement , arr[j]);
+                profit = (j-i+1)*maxelement + dp[j+1];
+                maxprofit = max(maxprofit , profit);
+
+            }
+            dp[i] = maxprofit;
+        }
+        return dp[0];
+    }
 public:
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
         vector<int>dp(n+1 , -1);
-        return solve(arr , 0 , n ,k ,dp);
+        // return solve(arr , 0 , n ,k ,dp);
+        return solvetab(arr, n , k);
     }
 };
