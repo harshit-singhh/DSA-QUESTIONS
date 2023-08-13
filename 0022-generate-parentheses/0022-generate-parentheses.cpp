928e9617cada3d1aa9680ee81fdd4fce4e5cc96e
Harshit Singh <harshitsingh81203@gmail.com>
2023-08-13 22:11:51 +0530

    Time: 2 ms (75.17%), Space: 16.2 MB (13.6%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    void solve(int index , int n , int oc , int cc , string temp , vector<string>&ans){
+        if(oc == 0 && cc == 0){
+            ans.push_back(temp);
+            return;
+        }
+
+        if(oc != 0){
+            temp.push_back('(');
+            solve(index+1 , n , oc-1 , cc , temp , ans);
+            temp.pop_back();
+        }
+        if(cc > oc){
+            temp.push_back(')');
+            solve(index+1 , n , oc , cc-1 , temp , ans);
+            temp.pop_back();
+        }
+    }
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string>ans;
+        string temp;
+
+        int opencount = n;
+        int closecount = n;
+
+        solve(0 , n , opencount , closecount , temp , ans);
+
+        return ans;
+    }
+};
