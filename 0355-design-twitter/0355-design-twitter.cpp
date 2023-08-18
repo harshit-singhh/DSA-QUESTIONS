a16b96cee836d3e3788818973598fcceaf806112
Harshit Singh <harshitsingh81203@gmail.com>
2023-08-18 16:18:44 +0530

    Time: 0 ms (100%), Space: 7.3 MB (25.87%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.cpp
+++ 0355-design-twitter/0355-design-twitter.cpp
@@ -1,47 +1,56 @@
-class Twitter {
-    private:
-    int timer = INT_MAX;
-    // used id - and its friends
-    unordered_map<int,unordered_set<int>>friends;
+class Node{
+    public:
+    int userId;
+    int tweetId;
+    Node*next;
 
-    //timer , {userd id , tweet id};
-    map<int , pair<int,int>> news;
+    Node(int Uid , int Tid){
+        this -> userId = Uid;
+        this -> tweetId = Tid;
+        this -> next = NULL;
+    }
+};
+
+class Twitter {
+    Node*head;
+
+    unordered_map<int , unordered_set<int>>follower;
 public:
     Twitter() {
-        
+        head = new Node(-1 , -1);
     }
     
     void postTweet(int userId, int tweetId) {
-        news[timer] = {userId , tweetId};
-        timer--;
+        Node*temp = new Node(userId , tweetId);
+        temp -> next = head -> next;
+        head -> next = temp;
     }
     
     vector<int> getNewsFeed(int userId) {
+        Node*temp = head->next;
         vector<int>ans;
-        int counter = 10;
-        for(auto it: news){
-            int timer = it.first;
-            int user = it.second.first;
-            int tweetId = it.second.second;
+        int counter = 0;
 
-            if(userId == user || (friends.find(userId) != friends.end() 
-            && friends[userId].find(user) != friends[userId].end())){
-                ans.push_back(tweetId);
-                counter--;
+        while(temp != NULL){
+            if(temp -> userId == userId || (follower.find(userId) != follower.end() && 
+            follower[userId].find(temp -> userId) != follower[userId].end())){
+                ans.push_back(temp -> tweetId);
+                counter++;
             }
-            if(counter == 0) break;
-
+            temp = temp -> next;
+            if(counter == 10) break;
         }
-
         return ans;
+
     }
     
     void follow(int followerId, int followeeId) {
-        friends[followerId].insert(followeeId);
+        follower[followerId].insert(followeeId);
     }
     
     void unfollow(int followerId, int followeeId) {
-        friends[followerId].erase(followeeId);
+        follower[followerId].erase(followeeId);
+        
     }
 };
 
@@ -53,3 +62,54 @@ public:
  * obj->follow(followerId,followeeId);
  * obj->unfollow(followerId,followeeId);
  */
+
+
+
+
+
+//  class Twitter {
+//     private:
+//     int timer = INT_MAX;
+//     // used id - and its friends
+//     unordered_map<int,unordered_set<int>>friends;
+
+//     //timer , {userd id , tweet id};
+//     map<int , pair<int,int>> news;
+// public:
+//     Twitter() {
+        
+//     }
+    
+//     void postTweet(int userId, int tweetId) {
+//         news[timer] = {userId , tweetId};
+//         timer--;
+//     }
+    
+//     vector<int> getNewsFeed(int userId) {
+//         vector<int>ans;
+//         int counter = 10;
+//         for(auto it: news){
+//             int timer = it.first;
+//             int user = it.second.first;
+//             int tweetId = it.second.second;
+
+//             if(userId == user || (friends.find(userId) != friends.end() 
+//             && friends[userId].find(user) != friends[userId].end())){
+//                 ans.push_back(tweetId);
+//                 counter--;
+//             }
+//             if(counter == 0) break;
+
+//         }
+
+//         return ans;
+//     }
+    
+//     void follow(int followerId, int followeeId) {
+//         friends[followerId].insert(followeeId);
+//     }
+    
+//     void unfollow(int followerId, int followeeId) {
+//         friends[followerId].erase(followeeId);
+//     }
+// };
