6754e9d86429c578f2bf3558f01dffc93ed45a0b
Harshit Singh <harshitsingh81203@gmail.com>
2023-08-22 14:09:26 +0530

    Time: 0 ms (100%), Space: 6 MB (10.53%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+         int low = 1;
+        int high = x;
+        
+        while(low <= high){
+             long long mid = low + (high - low) /2.0;
+            
+            if(mid*mid == x) return mid;
+            
+            else if(mid*mid > x){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        
+        return high;
+    }
+};
