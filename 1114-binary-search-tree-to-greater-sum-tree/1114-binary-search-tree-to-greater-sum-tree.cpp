f55966ee40448a6481d89d26b2a1b2ad68636a7f
Harshit Singh <harshitsingh81203@gmail.com>
2023-08-25 17:26:15 +0530

    Time: 0 ms (100%), Space: 8.5 MB (5.67%) - LeetHub

---
+++ 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    void findInorder(TreeNode*root , vector<int>&inorder){
+        if(root == NULL )return ;
+
+        findInorder(root -> left , inorder);
+        inorder.push_back(root -> val);
+        findInorder(root -> right , inorder);
+    }
+
+    void newtree(TreeNode*root , vector<int>&suffix , int &index){
+        if(root == NULL) return;
+
+        newtree(root -> left , suffix , index);
+        root -> val = suffix[index];
+        index++;
+        newtree(root -> right , suffix , index);
+    }
+public:
+    TreeNode* bstToGst(TreeNode* root) {
+        vector<int>inorder;
+
+        findInorder(root , inorder);
+        int n =inorder.size();
+
+        vector<int>suffix(n , 0);
+        int sum = 0;
+        for(int i = n-1 ; i >= 0 ; i--){
+            sum += inorder[i];
+            suffix[i] = sum;
+
+        }
+
+        int index = 0;
+        newtree(root , suffix , index);
+
+        return root;
+    }
+};
