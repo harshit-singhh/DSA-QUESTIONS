9978a50ded8aaf77e36b7d804e5a7faf023e2557
Harshit Singh <harshitsingh81203@gmail.com>
2023-08-29 12:17:30 +0530

    Time: 501 ms (5.02%), Space: 18.9 MB (26.64%) - LeetHub

---
+++ 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int bestClosingTime(string customers) {
+        int n = customers.size();
+
+        vector<int>preY(n+1);
+        preY[n] = 0;
+        vector<int>preN(n+1);
+
+
+        int count = 0;
+        for(int i = n-1 ; i>= 0 ; i--){
+            if(customers[i] == 'Y') count++;
+            preY[i] = count;
+        }
+        count = 0;
+        for(int i = 0 ; i < n ; i++){
+            preN[i] = count;
+            if(customers[i] == 'N') count++;
+        }
+        preN[n] = preN[n-1];
+
+        int min = INT_MAX;
+        int minindex = INT_MAX;
+        for(int i = 0 ; i <= n ; i++){
+            
+            int loss = preY[i] + preN[i];
+            
+
+
+            if(loss < min){
+                min = loss;
+                minindex = i;
+            }
+        }
+
+        for(auto it: preY) cout<<it<<" ";
+        cout<<endl;
+        for(auto it: preN) cout<<it<<" ";
+        cout<<endl;
+
+        return minindex;
+    }
+};
