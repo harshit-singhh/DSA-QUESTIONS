36657956f14f6580e5fb8504e26cb2d0bdaaedd1
Harshit Singh <harshitsingh81203@gmail.com>
2023-08-30 18:03:27 +0530

    Time: 27 ms (68.79%), Space: 71.3 MB (74.08%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,58 +1,79 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
+    public:
+    
+    
+    ListNode*reverse(ListNode*head){
+        ListNode*curr = head;
+        ListNode*prev = NULL;
+        ListNode*forward = NULL;
+        while(curr != NULL){
+            forward = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = forward;
+        }
+        return prev;
+    }
+    
+    void insertAttail(ListNode*&anshead, ListNode*&anstail,int digit){
+        ListNode*temp = new ListNode(digit);
+        if(anshead == NULL){
+            anshead = temp;
+            anstail = temp;
+            return;
+        }
+        else{
+            anstail -> next = temp;
+            anstail = anstail -> next;
+            // anstail = temp;
+            return;
+        }
+    }
+    
+    ListNode*add( ListNode*first,  ListNode*second){
+        int carry = 0;
+        int sum;
+        
+        ListNode*anshead = NULL;
+        ListNode*anstail = NULL;
+        
+        while(first != NULL || second != NULL || carry != 0){
+            int value1 = 0;
+            if(first != NULL){
+                value1 = first -> val;
+            }
+            int value2 = 0;
+            if(second != NULL){
+                value2 = second -> val;
+            }
+            
+            sum = carry + value1 + value2;
+            int digit = sum%10;
+            insertAttail(anshead,anstail,digit);
+            carry = sum/10;
+            
+            if(first != NULL){
+                first = first -> next;
+            }
+            if(second != NULL){
+                second = second -> next;
+            }
+        }
+            return anshead;
+    }
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int carry=0;
-        if(l1==nullptr)
-        return l2;
-        if(l2==nullptr)
-        return l1;
-        int sum=l1->val+l2->val;
-        if(sum>=10)
-        {carry=sum/10;
-        sum=sum%10;
-        }
-        ListNode * newlist=new ListNode(sum);
-        l1=l1->next;
-        l2=l2->next;
-        ListNode* newlist1=newlist;
-        while(l1!=nullptr && l2!=nullptr)
-        {
-             sum=carry+l1->val+l2->val;
-             carry=sum/10;
-             sum=sum%10;
-             ListNode* newnode=new ListNode(sum);
-             newlist1->next=newnode;
-             newlist1=newlist1->next;
-             l1=l1->next;
-             l2=l2->next;
-        }
-        while(l1!=nullptr)
-        {
-            sum=carry+l1->val;
-            carry=sum/10;
-            sum=sum%10;
-            ListNode* newnode =new ListNode(sum);
-            newlist1->next=newnode;
-            newlist1=newlist1->next;
-            l1=l1->next;
-        }
-        while(l2!=nullptr)
-        {
-            sum=carry+l2->val;
-            carry=sum/10;
-            sum=sum%10;
-            ListNode* newnode =new ListNode(sum);
-            newlist1->next=newnode;
-            newlist1=newlist1->next;
-            l2=l2->next;
-        }
-        if(carry!=0)
-        {
-            ListNode* newnode = new ListNode(carry);
-            newlist1->next=newnode;
-            newlist1=newlist1->next;
-        }
-
-        return newlist;
+        ListNode*head = add(l1 , l2);
+        return head;
     }
 };
