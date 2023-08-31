f9cc586894adf8e32df877a19eccbe4173d75e08
Harshit Singh <harshitsingh81203@gmail.com>
2023-08-31 10:38:16 +0530

    Time: 24 ms (70.96%), Space: 14.2 MB (75.48%) - LeetHub

--- 1451-minimum-number-of-taps-to-open-to-water-a-garden/1451-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1451-minimum-number-of-taps-to-open-to-water-a-garden/1451-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -4,10 +4,12 @@ public:
         int max = 0;
         int min = 0;
         int count = 0;
+        int index = 0;
         while(max < n){
-           for(int i = 0 ; i < n+1; i++){
+           for(int i = index ; i < n+1; i++){
                if(i - ranges[i] <= min  &&  i + ranges[i]> max){
                    max = i + ranges[i];
+                   index = i;
                }
            }
            if(min == max) return -1;
