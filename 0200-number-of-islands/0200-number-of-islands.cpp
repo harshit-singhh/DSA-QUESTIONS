718e4045077354fc57368994730bb979de24e001
Harshit Singh <harshitsingh81203@gmail.com>
2023-09-08 01:53:36 +0530

    Time: 28 ms (76.8%), Space: 13.3 MB (43.36%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,72 @@
+class Solution {
+     bool isValid(int currRow , int currCol , int row , int col){
+        if(currRow >= 0 && currRow < row && currCol >= 0 && currCol < col) return true;
+        return false;
+    }
+
+    void dfs(vector<vector<char>>& grid , vector<vector<int>>&visited , int currRow, int currCol,
+    int row , int col){
+        visited[currRow][currCol] = 1;
+
+        int drow[] = {-1 , 0 , 1 , 0};
+        int dcol[] = {0 , -1 , 0 , 1};
+
+        for(int i = 0 ; i < 4 ; i++){
+            int newrow = currRow + drow[i];
+            int newcol = currCol + dcol[i];
+
+            if(isValid(newrow , newcol , row, col) && grid[newrow][newcol] == '1' &&
+             visited[newrow][newcol] == 0){
+                 dfs(grid , visited , newrow , newcol , row , col);
+             }
+        }
+    }
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int count = 0;
+
+        int row = grid.size();
+        int col = grid[0].size();
+        vector<vector<int>>visited(row , vector<int>(col , 0));
+
+        // top and bottom row
+
+        for(int i = 0 ; i < col; i++){
+            
+            if(grid[0][i] == '1' && visited[0][i] == 0){
+                count++;
+                dfs(grid, visited , 0 , i , row , col);
+            }
+
+            if(grid[row-1][i] == '1' && visited[row-1][i] == 0){
+                count++;
+                dfs(grid, visited, row-1 , i , row , col);
+            }
+        }
+
+        // left and right col
+
+        for(int i = 0 ; i < row ; i++){
+            if(grid[i][0] == '1' && visited[i][0] == 0){
+                count++;
+                dfs(grid, visited , i , 0 , row , col);
+            }
+
+            if(grid[i][col-1] == '1' && visited[i][col-1] == 0){
+                count++;
+                dfs(grid , visited , i , col-1 , row , col);
+            } 
+        }
+
+        for(int i = 0 ; i < row ; i++){
+            for(int j = 0 ; j < col ; j++){
+                if(grid[i][j] == '1' && visited[i][j] == 0){
+                    count++;
+                    dfs(grid , visited , i , j , row , col);
+                }
+            }
+        }
+
+        return count;
+    }
+};
