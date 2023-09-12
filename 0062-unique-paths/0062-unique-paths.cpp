ea947c40bd00c6f304b6730c5f325f74d07c3e7b
Harshit Singh <harshitsingh81203@gmail.com>
2023-09-12 23:36:54 +0530

    Time: 3 ms (29.15%), Space: 6.7 MB (8.33%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -19,9 +19,10 @@ class Solution {
     // tabulation
 
     int solve(int m , int n ){
-        vector<vector<int>>dp(m , vector<int>(n+1 , -1));
+        vector<vector<int>>dp(m , vector<int>(n , -1));
 
         dp[0][0] = 1;
+
 
         for(int row = 0 ; row< m ; row++){
             for(int col = 0 ; col < n ; col++){
@@ -40,12 +41,43 @@ class Solution {
 
         return dp[m-1][n-1];
     }
+
+    int space(int n , int m){
+
+        vector<int>prev(m , 0);
+        for(int row = 0 ; row< n ; row++){
+            vector<int>curr(m , 0);
+            for(int col = 0 ; col < m ; col++){
+                if(row == 0 && col == 0 ) curr[col] = 1;
+                else{
+
+                    int ways1 = 0;
+                    if(row -1 >= 0)
+                    ways1 = prev[col];
+                    
+                    int ways2 = 0;
+                    if(col - 1 >= 0)
+                    ways2 = curr[col-1];
+                    
+                    curr[col] = (ways1 + ways2);
+                }
+            }
+            prev = curr;
+
+        }
+
+        return prev[m-1];
+
+
+        
+    }
 public:
     int uniquePaths(int m, int n) {
         
         
         vector<vector<int>>dp(m , vector<int>(n , -1));
         // return solve(m-1 , n-1 , dp);
-        return solve(m , n);
+        // return solve(m , n);
+        return space(m , n);
     }
 };
