e1a0dfc047e1a9e229cbc5a4fa02b33537b0f6fb
Harshit Singh <harshitsingh81203@gmail.com>
2023-09-16 23:16:27 +0530

    Time: 238 ms (12.5%), Space: 166.3 MB (87.5%) - LeetHub

---
+++ 3081-minimum-array-length-after-pair-removals/3081-minimum-array-length-after-pair-removals.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int minLengthAfterRemovals(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        int n = nums.size();
+        for(int i = 0 ; i < n ; i++){
+            mp[nums[i]]+=1;
+        }
+        
+        priority_queue<int>q;
+        
+        for(auto it: mp){
+            q.push(it.second);
+        }
+        
+        int size = q.size();
+        
+        while(q.size() > 1){
+            int t1 = q.top();
+            q.pop();
+            int t2 = q.top();
+            q.pop();
+            
+            
+            t1--;
+            t2--;
+            if(t1) q.push(t1);
+            if(t2) q.push(t2);
+            
+        }
+        
+        if(q.size() == 1) return q.top();
+        else return 0;
+    }
+};
