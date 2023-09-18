c369eee0297d349d63a1a8b419753e39c17079b3
Harshit Singh <harshitsingh81203@gmail.com>
2023-09-18 13:03:12 +0530

    Time: 34 ms (68.06%), Space: 9.3 MB (13.2%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -13,7 +13,7 @@ public:
 
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j< i ; j++){
-                if(nums[i]%nums[j] == 0 || nums[j]%nums[i] == 0){
+                if(nums[i]%nums[j] == 0){
 
                    if(dp[j] + 1 > dp[i]){
                        dp[i] = dp[j] + 1;
