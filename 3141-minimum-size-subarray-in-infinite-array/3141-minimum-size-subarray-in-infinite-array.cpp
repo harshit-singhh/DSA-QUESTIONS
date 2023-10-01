7c46935bc64a6b3d8cbb7a04daff9a79afd52536
Harshit Singh <harshitsingh81203@gmail.com>
2023-10-01 21:14:37 +0530

    Time: 49 ms (100%), Space: 45.4 MB (57.14%) - LeetHub

--- 3141-minimum-size-subarray-in-infinite-array/3141-minimum-size-subarray-in-infinite-array.cpp
+++ 3141-minimum-size-subarray-in-infinite-array/3141-minimum-size-subarray-in-infinite-array.cpp
@@ -2,58 +2,109 @@ class Solution {
 public:
     int minSizeSubarray(vector<int>& nums, int target) {
         
-        long long sum = 0;
-        int size = nums.size();
+    //     long long sum = 0;
+    //     int size = nums.size();
         
-        vector<int>cums;
-        for(auto it: nums){
-            cums.push_back(it);
-        }
-        for(auto it: nums){
-            cums.push_back(it);
+    //     vector<int>cums;
+    //     for(auto it: nums){
+    //         cums.push_back(it);
+    //     }
+    //     for(auto it: nums){
+    //         cums.push_back(it);
+    //     }
+
+    //     int n = cums.size();
+    //     sum = accumulate(cums.begin() , cums.end() , 0LL);
+    //     long long numsum = sum/2;
+
+    //     int count = 0;
+    //     if(target == numsum )return size;
+    //     while(target > numsum){
+    //         target -= numsum;
+    //         count+=size;
+    //         if(target == numsum){
+    //             count+=size;
+    //             return count;
+    //         }
+    //     }
+
+    //     int left = 0 , right = 0;
+    //     int minLen = INT_MAX;
+       
+    //     int currSum = 0;
+    //     while(right < n){
+    //         currSum += cums[right];
+            
+    //         while(currSum > target){
+    //             currSum -= cums[left];
+                
+    //             left++;
+    //         }
+
+    //         if(currSum == target){
+    //             minLen = min(minLen , right - left + 1);
+    //         }
+
+    //         right++;
+
+    //     }
+
+    //     if(minLen == INT_MAX) return -1;
+    //     count+= minLen;
+
+    //     return count;
+
+        
+    // }
+
+
+        int n = nums.size();
+
+        long long sum = 0;
+        sum = accumulate(nums.begin() , nums.end() , 0LL);
+        for(int i = 0 ; i < n ; i++){
+            nums.push_back(nums[i]);
         }
 
-        int n = cums.size();
-        sum = accumulate(cums.begin() , cums.end() , 0LL);
-        long long numsum = sum/2;
+        if(target == sum) return n;
+        
+        int full_array = target / sum;
 
         int count = 0;
-        if(target == numsum )return size;
-        while(target > numsum){
-            target -= numsum;
-            count+=size;
-            if(target == numsum){
-                count+=size;
-                return count;
-            }
-        }
 
-        int left = 0 , right = 0;
-        int minLen = INT_MAX;
+        count = full_array*n;
+
+        target = target%sum;
+
+        int left = 0  , right = 0;
+
+        int currsum = 0;
        
-        int currSum = 0;
-        while(right < n){
-            currSum += cums[right];
-            
-            while(currSum > target){
-                currSum -= cums[left];
-                
+        int minLen = INT_MAX;
+
+        while(right < 2*n){
+            currsum += nums[right%n];
+
+            while(currsum > target){
+                currsum -= nums[left%n];
                 left++;
             }
 
-            if(currSum == target){
+            if(currsum == target){
                 minLen = min(minLen , right - left + 1);
             }
 
             right++;
-
         }
 
         if(minLen == INT_MAX) return -1;
+
         count+= minLen;
 
         return count;
 
-        
     }
+
+
+
 };
