c349570db3a59c567203005097cc2b8cf4f7331c
Harshit Singh <harshitsingh81203@gmail.com>
2023-10-07 15:48:17 +0530

    Time: 0 ms (100%), Space: 8.1 MB (7.25%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -11,42 +11,68 @@
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        if(head == NULL || head -> next== NULL) return head;
-        ListNode*head1 = head;
-        ListNode*temp1 = head1;
-        ListNode*head2 = head -> next;
-        ListNode*temp2 = head2;
         
-        while(temp1 -> next != NULL && temp2 -> next != NULL){
-            temp1 -> next = temp2 -> next;
-            temp1 = temp1 -> next;
-            temp2 -> next = temp1 -> next;
-            temp2 = temp2 -> next;
+        if(head == NULL || head -> next == NULL) return head;
+
+        ListNode*curr = head;
+        ListNode*prev = NULL;
+        ListNode*forward = NULL;
+        int k = 2;
+        while(curr!= NULL && k!= 0 ){
+            forward = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = forward;
+            k--;
         }
-        
-        if(temp1 != NULL)temp1 -> next = NULL;
-        if(temp2 != NULL)temp2 -> next = NULL;
-        
-        temp1 = head1;
-        temp2 = head2;
-        
-        while(temp2!= NULL && temp1 != NULL){
-            
-            if(temp2 -> next == NULL){
-                temp2 -> next = temp1;
-                break;
-            }
-            
-            ListNode*next = temp2 -> next;
-            temp2 -> next = temp1;
-            temp2 = next;
-            next = temp1 -> next;
-            temp1 -> next = temp2;
-            temp1 = next;
-        }
-        
-        
-        
-        return head2;
+
+        head -> next = swapPairs(curr);
+
+        return prev;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     ListNode* swapPairs(ListNode* head) {
+//         if(head == NULL || head -> next== NULL) return head;
+//         ListNode*head1 = head;
+//         ListNode*temp1 = head1;
+//         ListNode*head2 = head -> next;
+//         ListNode*temp2 = head2;
+        
+//         while(temp1 -> next != NULL && temp2 -> next != NULL){
+//             temp1 -> next = temp2 -> next;
+//             temp1 = temp1 -> next;
+//             temp2 -> next = temp1 -> next;
+//             temp2 = temp2 -> next;
+//         }
+        
+//         if(temp1 != NULL)temp1 -> next = NULL;
+//         if(temp2 != NULL)temp2 -> next = NULL;
+        
+//         temp1 = head1;
+//         temp2 = head2;
+        
+//         while(temp2!= NULL && temp1 != NULL){
+            
+//             if(temp2 -> next == NULL){
+//                 temp2 -> next = temp1;
+//                 break;
+//             }
+            
+//             ListNode*next = temp2 -> next;
+//             temp2 -> next = temp1;
+//             temp2 = next;
+//             next = temp1 -> next;
+//             temp1 -> next = temp2;
+//             temp1 = next;
+//         }
+        
+        
+        
+//         return head2;
+//     }
+// };
