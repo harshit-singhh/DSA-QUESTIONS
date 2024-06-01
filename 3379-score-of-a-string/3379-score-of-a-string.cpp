efc70f8cf0239bcaed75a8369d237cc334c7e306
Harshit Singh <harshitsingh81203@gmail.com>
2024-06-01 10:11:46 +0530

    Time: 0 ms (100%), Space: 7.8 MB (42.17%) - LeetHub

---
+++ 3379-score-of-a-string/3379-score-of-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        
+        int n = s.size();
+
+        int sum = 0;
+
+        for(int i = 0 ; i < n-1 ; i++){
+            sum += abs(s[i] - s[i+1]);
+        }
+
+        return sum;
+    }
+};
