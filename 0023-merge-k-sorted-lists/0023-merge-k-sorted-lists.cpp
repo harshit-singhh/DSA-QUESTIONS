d7dbd53050c74e7a021e26d91157a4377d2c034c
Harshit Singh <harshitsingh81203@gmail.com>
2024-07-11 23:47:46 +0530

    Time: 156 ms (10.49%), Space: 351 MB (5.07%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,38 +10,42 @@
  */
 class Solution {
 public:
+
+    ListNode*merge(ListNode*head1 , ListNode*head2){
+        if(head1 == NULL) return head2;
+        if(head2 == NULL )return head1;
+
+        if(head1-> val <= head2 -> val){
+            head1-> next = merge(head1->next , head2);
+            return head1;
+        }
+        else{
+            head2-> next = merge(head2-> next , head1);
+            return head2;
+        }
+        return NULL;
+    }
+
+    ListNode* breakk(vector<ListNode*>lists , int start , int end){
+        if(start == end) return lists[start];
+
+        int mid = start + (end - start )/2;
+
+        ListNode*left = breakk(lists , start , mid);
+        ListNode*right = breakk(lists,mid+1 , end);
+
+        return merge(left , right);
+    }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         
-        int n  = lists.size();
+        int n = lists.size();
+        
+        
+        if (n == 0) return nullptr;
+        return breakk(lists, 0 , n-1);
 
-        priority_queue<pair<int , ListNode*> , vector<pair<int , ListNode*>> , greater<pair<int,ListNode*>>>q;
+        
 
-        for(int i = 0 ; i < n ;i++){
 
-            if(lists[i] != NULL){
-
-                ListNode*temp = lists[i];
-                while(temp != NULL){
-
-                    int data = temp -> val;
-                    q.push({data , temp});
-                    temp = temp -> next;
-                }
-
-            }
-        }
-
-        ListNode*head = new ListNode(-1);
-        ListNode*temp = head;
-
-        while(!q.empty()){
-            auto it = q.top();
-            q.pop();
-
-            temp -> next = it.second;
-            temp = temp -> next;
-        }
-
-        return head-> next;
     }
 };
