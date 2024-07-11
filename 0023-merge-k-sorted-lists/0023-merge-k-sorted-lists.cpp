a492ae3f52401869fc3b84b0e5ed36c640189029
Harshit Singh <harshitsingh81203@gmail.com>
2024-07-11 23:14:32 +0530

    Time: 18 ms (45.38%), Space: 19.1 MB (5.51%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -8,77 +8,40 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
-
- // heap method
-
-//  class comp{
-//      public:
-//      bool operator()(const ListNode*a , const ListNode*b){
-//          return a -> val > b -> val;
-//      }
-//  };
 class Solution {
-     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1 == NULL) return list2;
-        if(list2 == NULL) return list1;
-
-        if(list1-> val <= list2 -> val){
-            list1 -> next = mergeTwoLists(list1->next , list2);
-            return list1;
-        }
-        else{
-            list2-> next = mergeTwoLists(list2 -> next , list1);
-            return list2;
-        }
-        return NULL;
-    }
-
-    ListNode*breakk(int start , int end , vector<ListNode*>& lists){
-        if(start > end) return NULL;
-        if(start == end) return lists[start];
-
-        int mid = start + (end - start)/2;
-
-        ListNode*left = breakk(start , mid , lists );
-        ListNode*right = breakk(mid+1 , end ,  lists);
-
-        return mergeTwoLists(left , right);
-    }
-
 public:
-// heap method
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        
+        int n  = lists.size();
 
+        priority_queue<pair<int , ListNode*> , vector<pair<int , ListNode*>> , greater<pair<int,ListNode*>>>q;
 
+        for(int i = 0 ; i < n ;i++){
 
-//     ListNode* mergeKLists(vector<ListNode*>& lists) {
-//         priority_queue<ListNode* , vector<ListNode*> , comp> p;
+            if(lists[i] != NULL){
 
-//         for(auto it: lists){
-//             ListNode*temp = it;
-//             while(temp!= NULL){
-//                 p.push(temp);
-//                 temp = temp-> next;
-//             }
-//         }
+                ListNode*temp = lists[i];
+                while(temp != NULL){
 
-//         ListNode*head = new ListNode(-1);
-//         ListNode*temp = head;
+                    int data = temp -> val;
+                    q.push({data , temp});
+                    temp = temp -> next;
+                }
 
-//         while(!p.empty()){
-//             temp -> next = p.top();
-//             temp = temp -> next;
-//             p.pop();
-//         }
+            }
+        }
 
-//         temp -> next = NULL;
-//         return head -> next;
-//     }
-// };
+        ListNode*head = new ListNode(-1);
+        ListNode*temp = head;
 
-    ListNode* mergeKLists(vector<ListNode*>& lists){
-        ListNode* head = breakk(0 , lists.size()- 1 , lists);
-        return head;
- 
+        while(!q.empty()){
+            auto it = q.top();
+            q.pop();
+
+            temp -> next = it.second;
+            temp = temp -> next;
+        }
+
+        return head-> next;
     }
 };
