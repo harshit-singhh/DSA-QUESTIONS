13a363b8a506eb43c8b1bc58233df73c78f0bd44
Harshit Singh <harshitsingh81203@gmail.com>
2024-09-18 23:37:10 +0530

    Time: 8 ms (48.02%), Space: 15.6 MB (70.19%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,37 +1,30 @@
+bool compare(int a, int b) {
+    string sa = to_string(a);
+    string sb = to_string(b);
+
+    // Custom comparison to check which concatenation is larger
+    if((sa + sb )> (sb + sa)){
+        return true;
+    }
+    return false;
+}
+
 class Solution {
 public:
     string largestNumber(vector<int>& nums) {
-        
-        int n = nums.size();
+        // Sort using the custom comparator
+        sort(nums.begin(), nums.end(), compare);
 
-        for(int i = 0 ; i < n-1 ; i++){
-            for(int j = i+1 ; j < n ; j++){
-
-                string a = to_string(nums[i]);
-                string b = to_string(nums[j]);
-
-                string append1 = a+b;
-                string append2 = b+a;
-
-                if(append2 > append1) swap(nums[j] , nums[i]);
-            }
+        // If the largest number after sorting is '0', the result is "0"
+        if (nums[0] == 0) {
+            return "0";
         }
 
         string ans = "";
-
-        for(int i = 0 ; i< n ; i++){
-            ans += to_string(nums[i]);
-        }
-        bool allzero = true;
-
-        for(auto it: ans){
-            if(it != '0'){
-                allzero = false;
-                break;
-            } 
+        for (int num : nums) {
+            ans += to_string(num);
         }
 
-        if(allzero) return "0";
         return ans;
     }
 };
