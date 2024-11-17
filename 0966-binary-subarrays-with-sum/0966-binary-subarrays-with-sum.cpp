d2d7a66a1c54a1be7d03537f2bdf3c66e913e9c3
Harshit Singh <harshitsingh81203@gmail.com>
2024-11-17 21:03:33 +0530

    Time: 0 ms (100%), Space: 33.5 MB (37.6%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -1,32 +1,27 @@
 class Solution {
-     int Atmost(vector<int>& arr, int n, int target){
-        
+public:
+    
+    int solve(vector<int>nums , int n , int goal){
+        int left = 0, right = 0;
+        if(goal <0  ) return 0;
+        int count = 0;
         int sum = 0;
-        if(target < 0) return 0;
-        
-        int totalSubarrays = 0;
-        
-        int left = 0 , right = 0;
-        
         while(right < n){
-            
-            sum+=arr[right];
-            
-            while(sum > target){
-                sum -= arr[left];
+            sum+=nums[right];
+            while(sum > goal){
+                sum -= nums[left];
                 left++;
             }
-            
-            totalSubarrays += right - left +1; // we can use this formula also to calculate the 
-            // total number of subarrays between right and left
+
+            count+= right-left+1;
             right++;
         }
-        
-        return totalSubarrays;
+        return count;
     }
-public:
-    int numSubarraysWithSum(vector<int>& arr, int target) {
-        int n = arr.size();
-         return Atmost(arr,n,target) - Atmost(arr,n,target-1);
+
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int n = nums.size();
+
+        return solve(nums,n, goal) - solve(nums,n,goal-1);
     }
 };
