06d9698d4c79b0adc10481cef32ff529cf5f0808
Harshit Singh <harshitsingh81203@gmail.com>
2024-11-18 20:41:15 +0530

    Time: 31 ms (37.77%), Space: 12.3 MB (79.47%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,74 +1,41 @@
 class Solution {
-     private:
-    // int solve( string &s1 , string &s2 , int n , int m ,vector<vector<int>>&dp){
-        
-    //     if(n == 0) return 0; //instead of n== -1 , we did n== 0 because we shifted the index
-    //     if(m == 0) return 0;
-        
-    //     if(dp[n][m] != -1) return dp[n][m];
-        
-    //     if(s1[n-1] == s2[m-1]){ // because we have shifted , so we did this insted of s1[n] == s2[m];
-    //         return dp[n][m] = 1 + solve(s1 , s2 , n-1 , m-1 ,dp);
-    //     }
-        
-    //     else return dp[n][m] = max(solve(s1 , s2 , n-1 , m ,dp) , solve(s1 ,s2 , n , m-1 ,dp));
-    // }
-
-
-    //tabulation - we shifted the index in this question to perform our tabulation
-    // therefore memoization code is also changed a little bit
-
-    int solvetab(string &s1 , string &s2 , int n , int m){
-        vector<vector<int>>dp(n+1 , vector<int>(m+1 , -1));
-        // now base case is at index 0 , so we can show in dp matrix
-        // other wise it was difficult to show -ve index in dp matrix
-        
-        for(int i = 0 ; i <= m ; i++) dp[0][i] = 0;
-        
-        for(int i = 0 ; i <= n ; i++) dp[i][0] = 0;
-
-        for(int i = 1 ; i<= n ; i++){
-            for(int j = 1 ; j<= m ; j++){
-                 if(s1[i-1] == s2[j-1]){ 
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                }
-        
-                else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
-            }
-        }
-
-        return dp[n][m];
-
-    }
-
-
-    int space(string s1 , string s2 , int n , int m){
-
-        vector<int>prev(m +1 , 0);
-        vector<int>curr(m + 1 , 0);
-
-        for(int i = 1 ; i<= n ; i++){
-            for(int j = 1 ; j<= m ; j++){
-                 if(s1[i-1] == s2[j-1]){ 
-                    curr[j] = 1 + prev[j-1];
-                }
-        
-                else curr[j] = max(prev[j],curr[j-1]);
-            }
-            prev = curr;
-        }
-
-        return prev[m];
-    }
-
 public:
+    int dp[1005][1005]; // Adjust size for offsets
+
+    int solve(string& str1, string& str2, int index1, int index2) {
+        // Base case: if either string is exhausted
+        if (index1 == -1 || index2 == -1) return 0;
+
+        // Apply consistent offset
+        int offset1 = index1 + 3;
+        int offset2 = index2 + 3;
+
+        // Check memoization
+        if (dp[offset1][offset2] != -1) return dp[offset1][offset2];
+
+        // Calculate LCS
+        int matching = 0, notmatching = 0;
+
+        if (str1[index1] == str2[index2]) {
+            matching = 1 + solve(str1, str2, index1 - 1, index2 - 1);
+        } else {
+            notmatching = max(
+                solve(str1, str2, index1 - 1, index2), 
+                solve(str1, str2, index1, index2 - 1)
+            );
+        }
+
+        // Store result in `dp` using offset
+        return dp[offset1][offset2] = max(matching, notmatching);
+    }
+
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.size();
         int m = text2.size();
-        vector<vector<int>>dp(n+1 , vector<int>(m+1 , -1));
-        // return solve(text1 , text2 , n , m ,dp); // to shift the index . n- 1 = n , m-1 = m
 
-        // return solvetab(text1 , text2 , n , m);
-        return space(text1 , text2 , n , m );
+        // Initialize `dp` with -2
+        memset(dp, -1, sizeof(dp));
+
+        return solve(text1, text2, n - 1, m - 1);
     }
 };
