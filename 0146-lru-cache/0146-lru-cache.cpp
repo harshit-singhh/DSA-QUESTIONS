c7d18fb31fd874b486e288699ede5aa2e415fd25
Harshit Singh <harshitsingh81203@gmail.com>
2026-09-20 16:27:16 +0530

    Time: 18 ms (73.34%), Space: 48 MB (87.8%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,72 +1,77 @@
-class LRUCache {
+class Node {
 public:
+    int key, value;
+    Node* next = NULL;
+    Node* prev = NULL;
 
-// Make a class of Node which contain key,value,Prev ptr,next ptr.
-class Node{
-public:
-    int key,val;
-    Node* prev;
-    Node* next;
-    Node(int _key,int _val){
-        key=_key;
-        val=_val;
+    Node(int a, int b) {
+        this->key = a;
+        this->value = b;
     }
 };
-int cap; // to check the capacity of the map
 
-unordered_map<int,Node*>mp; // make a map of int and Node* type , basically Node* stores the address of the node
-
-Node* head=new Node(-1,-1); // make a head with value -1,-1.
-Node* tail=new Node(-1,-1); // make a tail with value -1,-1.
+class LRUCache {
+public:
+    int cap;
+    Node* head = new Node(-1, -1);
+    Node* tail = new Node(-1, -1);
+    unordered_map<int, Node*> mp;
 
     LRUCache(int capacity) {
-        cap=capacity; // capacity scope in only in this loop so store it in cap
-        head->next=tail; // make a link head->tail and tail->prev becaue of DLL(double Linked list)
-        tail->prev=head;
-    }
-// Do dry for better understanding
-
-    // I need to add newNode right after the head,
-    void addNode(Node* newNode){
-        Node* temp=head->next; // store the head next 
-        // and then made 4 links, because of dll
-        head->next=newNode;
-        newNode->prev=head;
-        newNode->next=temp;
-        temp->prev=newNode;
+        this->cap = capacity;
+        // Connect dummy head and tail
+        head->next = tail;
+        tail->prev = head;
     }
 
-    void deleteNode(Node* delNode){
-        // store prev of delNode and nextNode and then delete and form the link For DLL.
-        Node* delPrev=delNode->prev;
-        Node* delNext=delNode->next;
-        delPrev->next=delNode->next;
-        delNext->prev=delNode->prev;
+    // Helper: Add node right after head
+    void addNode(Node* newnode) {
+        Node* temp = head->next;
+        newnode->next = temp;
+        newnode->prev = head;
+        head->next = newnode;
+        temp->prev = newnode;
+    }
+
+    // Helper: Unlink a node from the list
+    void deleteNode(Node* delnode) {
+        Node* delprev = delnode->prev;
+        Node* delnext = delnode->next;
+        delprev->next = delnext;
+        delnext->prev = delprev;
     }
 
     int get(int key) {
-        if(mp.find(key)!=mp.end()){ // if it is present in map
-            Node* resNode=mp[key]; // store the address of mp of key
-            int res=resNode->val; // store the val of the address
-            deleteNode(resNode); // delete that node
-            addNode(resNode); // add that node 
-            mp[key]=head->next; // now mp of key contain new address
-            return res;
-        }   
-        return -1;  // if not present in map
+        if (mp.find(key) == mp.end()) return -1;
+
+        Node* resnode = mp[key];
+        deleteNode(resnode);
+        addNode(resnode);
+
+        return resnode->value;
     }
-    
+
     void put(int key, int value) {
-        if(mp.find(key)!=mp.end()){ // if it is already in map 
-            Node* existingNode=mp[key]; // fin d the address
-            mp.erase(key);// erase it from map
-            deleteNode(existingNode); // delete it 
+        // Case 1: Key already exists -> update value and move to head
+        if (mp.find(key) != mp.end()) {
+            Node* existingNode = mp[key];
+            existingNode->value = value;
+            deleteNode(existingNode);
+            addNode(existingNode);
+            return;
         }
-        if(mp.size()==cap){ // it size equals delete the LRU node which is at tail->prev
-            mp.erase(tail->prev->key); 
-            deleteNode(tail->prev);
+
+        // Case 2: Capacity reached -> evict LRU node (tail->prev)
+        if (mp.size() == cap) {
+            Node* lruNode = tail->prev;
+            mp.erase(lruNode->key);
+            deleteNode(lruNode);
+            delete lruNode; // Free memory
         }
-        addNode(new Node(key,value)); // add then new node with the key,value right after head
-        mp[key]=head->next; // change the new address of map of key with head->next
+
+        // Insert brand new key
+        Node* newNode = new Node(key, value);
+        addNode(newNode);
+        mp[key] = newNode;
     }
 };
