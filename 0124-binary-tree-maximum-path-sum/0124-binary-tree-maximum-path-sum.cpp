96b0ffda606214d72b4761fb45978fe4330f5bbf
Harshit Singh <harshitsingh81203@gmail.com>
2026-09-26 23:03:32 +0530

    Time: 0 ms (100%), Space: 27.9 MB (77.28%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -10,31 +10,25 @@
  * };
  */
 class Solution {
-
-    private:
-    int findsum(TreeNode*root , int& maxSum){
-        if(root == NULL) return 0;
-        
-        int left = findsum(root -> left , maxSum);
-        int right = findsum(root ->right , maxSum);
-        
-        int sum = max(left,right)+root->val; // ya to node left side se aane wale sum max hoga ya fir node + right side
-        
-        int sum2 = left+right+root->val; // ya ho sakta he left + node + right side ka sum max ho
-        
-        int sum3 = root->val; // ya ho sakta he ki left, right side se -ve values aa rhi ho, to us case me agar node ki value
-        // plus dono me se kisi side se bhi aane wale data ko add krenge to aur -ve number aa jaega. there fore in this case 
-        
-        // node ka data will be max
-        
-        maxSum = max(maxSum,max(sum,max(sum2,sum3)));
-        
-        return max(sum,sum3);
-    }
 public:
+    int solve(TreeNode*root , int&maxi){
+        if(root == NULL ) return 0;
+
+        int leftsum = solve(root -> left , maxi);
+        int rightsum = solve(root -> right , maxi);
+
+        int rootVal= root -> val;
+
+        int sum1 = rootVal;
+        int sum2 = rootVal + max(leftsum , rightsum);
+        int sum3 = rootVal + leftsum + rightsum;
+
+        maxi = max(maxi , max(sum1 , max(sum2 , sum3)));
+        return max(sum1 , sum2);
+    }
     int maxPathSum(TreeNode* root) {
-        int maxSum = INT_MIN;
-        findsum(root,maxSum);
-        return maxSum;
+        int maxi = INT_MIN;
+        int value = solve(root , maxi);
+        return maxi;
     }
 };
